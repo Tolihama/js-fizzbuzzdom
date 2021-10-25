731846a0685db26859d997d413b2e24881b52388
Tolihama <fra070@gmail.com>
2021-10-25 15:35:10 +0200

    Beta 2

--- js/main.js
+++ js/main.js
@@ -4,19 +4,16 @@
 
 // Selectors
 const container = document.querySelector('.container');
-const div = document.createElement('div');
 
 // Script parameters
 const size = 100;
 const rowSize = 7;
 
 // Dom elements generator
-
 for (let i = 1; i <= size;) {
     container.innerHTML += '<div class="row">';
     const row = document.querySelector('.container .row:last-child');
     for (let iRow = 1; (iRow <= rowSize) && (i <= size); iRow++) {
-
         if ((i % 3 === 0) && (i % 5 === 0)) {
             row.innerHTML += '<div class="square fizzbuzz">fizzbuzz</div>';
         } else if (i % 3 === 0) {
@@ -27,7 +24,6 @@ for (let i = 1; i <= size;) {
             row.innerHTML += `<div class="square">${i}</div>`;
         }
         i++;
-        console.log(i);
     }
     container.innerHTML += '</div>';
 }
